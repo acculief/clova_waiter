14544175a8264b9d74c8f5ce63fac763b5d09e9e
acculief <yajousaki@gmail.com>
2018-09-01 12:57:10 +0900

    aaaa

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const clovaSkillHandler = clova.Client
     });
   })
 
-  var orderName  = [];
-  var orderAmount = [];
-  var againScript = '';
+  let orderName  = [];
+  let orderAmount = [];
+  let againScript = '';
 
   // カスタムインテント or ビルトインインテント
   .onIntentRequest(responseHelper => {
