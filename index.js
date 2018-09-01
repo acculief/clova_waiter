9e79ecb81a543ea74d8710c1e763f8720772954a
acculief <yajousaki@gmail.com>
2018-09-01 12:40:14 +0900

    hge

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // 応答の最後に追加するテンプレート
-const TEMPLATE_INQUIRY = 'あいうえおかきくけこ';
+const TEMPLATE_INQUIRY = 'ご注文をどうぞ。';
 
 const clovaSkillHandler = clova.Client
   .configureSkill()
@@ -15,46 +15,37 @@ const clovaSkillHandler = clova.Client
       value: `いらっしゃいませ。${TEMPLATE_INQUIRY}`,
     });
   })
+
+  var orderName  = []
+  var orderAmount = []
+  var againScript = '';
+
   // カスタムインテント or ビルトインインテント
   .onIntentRequest(responseHelper => {
     const intent = responseHelper.getIntentName();
     let speech;
     switch (intent) {
-      // ユーザーのインプットが星座だと判別された場合。第2引数はreprompt(入力が行われなかった場合の聞き返し)をするか否か。省略可。
-      case 'FortuneIntent':
-        // 星座を取得
+      case 'MenuIntent':
         const slots = responseHelper.getSlots()
-        // Slotに登録されていない星座はnullになる
-        if(slots.zodiac_signs == null) {
+        if(slots.menuSlot == null) {
           speech = {
             lang: 'ja',
             type: 'PlainText',
-            value: `星座に誤りがあります。他の星座でお試し下さい。`
+            value: `もう一度お願いします。`
           }
           responseHelper.setSimpleSpeech(speech)
           responseHelper.setSimpleSpeech(speech, true)
-          // 下記でも可
-          /*
-          responseHelper.setSimpleSpeech(
-            clova.SpeechBuilder.createSpeechText(`星座に誤りがあります。他の星座でお試し下さい。`)
-          );
-          */
           break
         }
-        // 「中吉」だと「なかよし」発生されてしまう
-        const fortune = ['大吉', 'ちゅうきち', '小吉', '吉', '凶']
-        const lucky = ['赤', '青', '黄色', '緑', 'ピンク']
-        const zodiacSigns = ['牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座']
-        const message = [ '今日も１日頑張りましょう', 'お仕事頑張ってね', '暑いね', 'おやすみ' ]
-        // 日と星座を元に運勢を決定。日が変わると違う運勢に。
-        const fortuneToday = fortune[(new Date().getDate() + zodiacSigns.indexOf(slots.zodiac_signs)) % fortune.length]
-        const luckyToday = lucky[(new Date().getDate() + zodiacSigns.indexOf(slots.zodiac_signs)) % lucky.length]
-        const messageToday = message[(new Date().getDate() + zodiacSigns.indexOf(slots.zodiac_signs)) % message.length]
+
+        orderNameList = [];
+        orderName.push(orderNameList.indexOf(slots.menuSlot))
+        orderAmount.push(slots.menuSlot)
 
         speech = {
           lang: 'ja',
           type: 'PlainText',
-          value: `${slots.zodiac_signs}の今日の運勢は${fortuneToday}!。ラッキーカラーは${luckyToday}だよん。${messageToday}。${TEMPLATE_INQUIRY}`
+          value: `追加の注文をどうぞ`
         }
         responseHelper.setSimpleSpeech(speech)
         responseHelper.setSimpleSpeech(speech, true)
@@ -75,10 +66,14 @@ const clovaSkillHandler = clova.Client
       case 'Clova.YesIntent':
       case 'Clova.NoIntent':
       case 'Clova.CancelIntent':
+        for (var i = 0; i < orderName.length; i++) {
+          againScript += orderName + 'を' + 'orderAmount' + '個'
+        }
+        againScript += '以上でよろしいでしょうか。'
         speech = {
           lang: 'ja',
           type: 'PlainText',
-          value: `意図しない入力です。${TEMPLATE_INQUIRY}`
+          value: `ありがとうございます。復唱します。${againScript}`
         }
         responseHelper.setSimpleSpeech(speech)
         break;
@@ -96,9 +91,6 @@ const clovaMiddleware = clova.Middleware({
   applicationId: process.env.APPLICATION_ID
 });
 app.post('/clova', clovaMiddleware, clovaSkillHandler);
-
-// リクエストの検証を行わない
-//app.post('/clova', bodyParser.json(), clovaSkillHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
